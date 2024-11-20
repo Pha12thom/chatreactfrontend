348cb90a70b1e60070264b96d1c4185e4b0392d3
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-11-20 13:30:55 +0300

    add router sections

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom'
 import './App.css'
 import Chat from './pages/chat'
@@ -9,12 +8,12 @@ import Login from './pages/login'
 function App() {
   return (
     <>
-      
+      <Routes>
         <Route path="/" element={<Chat />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/chat" />} />
-
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
     </>
   )
 }

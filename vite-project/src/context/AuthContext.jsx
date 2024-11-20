c6d97c4f42d03b495447980dab2ea8b592f2f0cc
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-11-20 17:23:20 +0300

    context providers

--- vite-project/src/context/AuthContext.jsx
+++ vite-project/src/context/AuthContext.jsx
@@ -1,1 +1,14 @@
 import { createContext } from "react";
+export const AuthContext = createContext();
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState({
+        name: "Milugo",
+        
+    });
+
+  return <AuthContext.Provider 
+  value={{
+    user,
+    setUser
+  }}>{children}</AuthContext.Provider>;
+};
